12884365f64afb884352cab9856d67b5eaa4f453
silvanoamaral <silvano.amaral9@gmail.com>
2018-04-08 21:44:34 -0300

    Responsive Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
+/*
+  Referencia para responsive
+  https://medium.com/samsung-internet-dev/common-responsive-layouts-with-css-grid-and-some-without-245a862f48df
+*/
+
 const Home = () => (
   <div>
     <div className="banner-home">
@@ -7,6 +12,55 @@ const Home = () => (
       <p>Está à procura de um Front-End Developer criativo e experiente? Não procure mais :) pois eu estou pronto para desempenhar o cargo!</p>
     </div>
     <div className="content">
+      <div className="technology">
+        <h1 className="title">
+          tecnologia utilizada
+        </h1>
+        <section className="cards">          
+          <article>
+            <img className="article-img hide"src="https://www.infinitoag.com/servicos/programacao-web/" alt=" " />
+            <div className="article-img"></div>
+            <h1 className="article-title">
+              javascript / Typescript
+            </h1>
+          </article>
+          <article>
+            <img className="article-img hide" src="http://placekitten.com/320/220" alt=" " />
+            <div className="article-img"></div>
+            <h1 className="article-title">
+              React
+            </h1>
+          </article>
+          <article>
+            <img className="article-img hide" src="http://placekitten.com/330/240" alt=" " />
+            <div className="article-img"></div>
+            <h1 className="article-title">
+              Webpack
+            </h1>
+          </article>
+          <article>
+            <img className="article-img hide" src="http://placekitten.com/280/250" alt=" " />
+            <div className="article-img"></div>
+            <h1 className="article-title">
+              HTML
+            </h1>
+          </article>
+          <article>
+            <img className="article-img hide" src="http://placekitten.com/310/210" alt=" " />
+            <div className="article-img"></div>
+            <h1 className="article-title">
+              CSS
+            </h1>
+          </article>
+          <article>
+            <img className="article-img hide" src="http://placekitten.com/430/240" alt=" " />
+            <div className="article-img"></div>
+            <h1 className="article-title">
+              Title of article
+            </h1>
+          </article>      
+        </section> 
+      </div>
     </div>
   </div>
 )
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,7 +8,7 @@ const Navigation = () => (
         <nav className="wrapper">
             <div className="logo"></div>
             <input type="checkbox" id="menu-toggle" />
-            <label for="menu-toggle" className="label-toggle"></label>
+            <label htmlFor="menu-toggle" className="label-toggle"></label>
             <ul>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/roster'>Roster</Link></li>
